abc838f5e263c42082c324d84c1dcf47074656bc
Scott Murray <scott.murray@eegeo.com>
2015-09-01 12:28:29 +0100

    Remove references to Android from iOS API logger.

--- src/Logger.h
+++ src/Logger.h
@@ -2,11 +2,4 @@
 
 #pragma once
 
-#include "Types.h"
-
-#if defined(EEGEO_DROID)
-#include <android/log.h>
-#define EXAMPLE_LOG(...) __android_log_print(ANDROID_LOG_INFO,"Eegeo_Examples",__VA_ARGS__)
-#else
 #define EXAMPLE_LOG(...) printf(__VA_ARGS__)
-#endif
