d7535f762a1f5246b668179bca8fcb75b134983f
Paul Harris <paul.harris@eegeo.com>
2017-05-19 17:02:29 +0100

    Add docs for WRLDMarker.h and WRLDIndoorMap.h
    Buddy: Vimmy

--- src/public/WRLDIndoorMap.h
+++ src/public/WRLDIndoorMap.h
@@ -2,20 +2,34 @@
 
 #import <Foundation/Foundation.h>
 
+
 @interface WRLDIndoorMapFloor: NSObject
 
+/// A short string identifier for a floor, unique within its indoor map. For example, "G".
 @property (readonly) NSString* floorId;
+
+/// The human-readable name of the floor. For example, "Ground Floor".
 @property (readonly) NSString* name;
+
+/// The index of this floor relative to the bottom floor of its indoor map.
 @property (readonly) NSInteger floorIndex;
 
 @end
 
 
+/// An object containing information about an indoor map.
 @interface WRLDIndoorMap: NSObject
 
+/// A string representing a unique ID for this indoor map
 @property (readonly) NSString* indoorId;
+
+/// The human-readable name of this indoor map.
 @property (readonly) NSString* name;
+
+/// An array of WRLDIndoorMapFloor objects, corresponding to the floors of this indoor map.
 @property (readonly) NSArray<WRLDIndoorMapFloor*>* floors;
+
+/// Optional additional JSON data associated with this indoor map.
 @property (readonly) NSString* userData;
 
 @end
--- src/public/WRLDMarker.h
+++ src/public/WRLDMarker.h
@@ -4,38 +4,62 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+
+/// The method of determining a markers altitude.
 typedef NS_ENUM(NSInteger, MarkerElevationMode)
 {
+    /// A markers elevation should be relative to sea-level.
     HeightAboveSeaLevel,
+
+    /// A markers elevation should be relative to the ground directly below it.
     HeightAboveGround
 };
 
+
+/// A Marker is an icon placed at a point on or above the map’s surface.
+/// They can have some title text attached to them, and can additionally be placed indoors.
 @interface WRLDMarker : NSObject
 
 + (instancetype)markerAtCoordinate:(CLLocationCoordinate2D)coordinate;
 
+/*!
+ @brief Instantiate a marker at a given location, optionally indoors.
+ @param coordinate The coordinate to place this marker at.
+ @param indoorMapId The ID of an indoor map this marker should be inside. (See WRLDIndoorMap).
+ @param floorId An index specifying which floor of the indoor map this marker should be on. (See WRLDIndoorMap).
+ @returns A WRLDMarker instance.
+ */
 + (instancetype)markerAtCoordinate:(CLLocationCoordinate2D)coordinate
                        inIndoorMap:(NSString *)indoorMapId
                            onFloor:(NSInteger)floorId;
 
+/// The location of the marker.
 @property (nonatomic) CLLocationCoordinate2D coordinate;
 
+/// The height of the marker above either the ground, or sea-level, depending on the elevationMode property.
 @property (nonatomic) CLLocationDistance elevation;
 
+/// Whether this marker should be positioned relative to the ground, or sea-level.
 @property (nonatomic) MarkerElevationMode elevationMode;
 
+/// When markers overlap, this property determines which one appears on top of the other.
 @property (nonatomic) NSInteger drawOrder;
 
+/// The text label to display next to the marker.
 @property (nonatomic, copy) NSString* title;
 
 @property (nonatomic, readonly, copy) NSString* styleName;
 
+/// Optional JSON user data associated with this marker.
 @property (nonatomic, copy) NSString* userData;
 
+/// A string used to determine which icon is displayed for this marker.
 @property (nonatomic, copy) NSString* iconKey;
 
+/// The ID of the indoor map this marker is associated with - if any.
 @property (nonatomic, readonly, copy) NSString* indoorMapId;
 
+/// The index of the floor this marker is associated with - if any.
 @property (nonatomic, readonly) NSInteger indoorFloorId;
 
 @end
