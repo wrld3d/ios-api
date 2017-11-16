b4c6cf1fff520527e3d1fd2045c9125254005424
SamAins <sam.ainsworth@wrld3d.com>
2017-11-16 17:07:26 +0000

    Implenets SearchWidgetView
    Pair: Tom P

---
+++ WrldWidgets/Search/src/private/WRLDSearchWidgetView.m
@@ -0,0 +1,61 @@
+
+#import "WRLDSearchWidgetView.h"
+
+@interface WRLDSearchWidgetView()
+
+@property (strong, nonatomic) IBOutlet UIView *rootView;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@property (unsafe_unretained, nonatomic) IBOutlet UIButton *optionButton;
+@property (unsafe_unretained, nonatomic) IBOutlet UISearchBar *searchBar;
+
+- (IBAction)optionClicked:(id)sender;
+
+@end
+
+
+@implementation WRLDSearchWidgetView
+
+
+-(instancetype)initWithCoder:(NSCoder *)aDecoder{
+    self = [super initWithCoder:aDecoder];
+    if(self){
+        [self customInit];
+    }
+    
+    return self;
+}
+
+-(instancetype)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    
+    if(self){
+        [self customInit];
+    }
+    
+    return self;
+}
+
+-(void)customInit{
+    //NSString* mainBundlePath = [[NSBundle mainBundle]bundlePath];
+ 
+    //NSString* frameworkBundlePath = [[[NSBundle mainBundle] builtInPlugInsPath] @"WrldWidgets.bundle"];
+    //NSBundle* frameworkBundle = [NSBundle bundleWithPath:@"/Library/WrldWidgets.bundle"];
+    
+    NSBundle* widgetsBundle = [NSBundle bundleForClass:[WRLDSearchWidgetView class]];
+    
+    [widgetsBundle.self loadNibNamed:@"WRLDSearchWidgetView" owner:self options:nil];
+    
+    [self addSubview:self.rootView];
+    
+    self.rootView.frame = self.bounds;
+}
+
+
+-(void)setSearchModule:(WRLDSearchModule*) searchModule{
+    [_tableView setDataSource:searchModule];
+}
+
+- (IBAction)optionClicked:(id)sender {
+}
+@end
---
+++ WrldWidgets/Search/src/public/WRLDSearchWidgetView.h
@@ -0,0 +1,9 @@
+
+#import <UIKit/UIKit.h>
+#import "WRLDSearchModule.h"
+
+@interface WRLDSearchWidgetView : UIView
+
+-(void)setSearchModule:(WRLDSearchModule*) searchModule;
+
+@end
