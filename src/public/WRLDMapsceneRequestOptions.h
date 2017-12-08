3fcb57cff7c6491415ecbf6f43a3c7a5c1efac23
SamAins <sam.ainsworth@wrld3d.com>
2017-12-08 14:14:00 +0000

    WRLDMapsceneRequest & Options Documaentation
    Added Documentation for the Request and Request Options
    
    Buddy: Jonty D

--- src/public/WRLDMapsceneRequestOptions.h
+++ src/public/WRLDMapsceneRequestOptions.h
@@ -3,14 +3,25 @@
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
+/**
+ * A set of parameters for creating a Mapscene request.
+ */
 @interface WRLDMapsceneRequestOptions : NSObject
 
+/**
+ * @param shortLinkUrl The full url or the short link of the Mapscene to load.
+ * @param applyMapsceneOnSuccess .
+ * @returns Its self.
  */
 -(instancetype)initMapsceneRequestOptions :(NSString*)shortLinkUrl :(bool)applyMapsceneOnSuccess;
 
+/**
+ * @returns shortLinkUrl
  */
 -(NSString*)getShortLinkUrl;
 
+/**
+ * @returns applyMapsceneOnSuccess
  */
 -(bool)getApplyMapsceneOnSuccess;
 
