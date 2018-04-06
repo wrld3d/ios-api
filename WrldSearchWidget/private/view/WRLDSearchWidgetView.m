a0411874c0ba5d49dbdf36957ee0fe94b578a5f6
Michael O'Donnell <michael.odonnell@wrld3d.com>
2018-04-06 09:51:13 +0100

    Works around an issue in iOS 8 where UIViews start receiving pointInside calls for keyboard events if the application is backgrounded while the keyboard on screen.  These events are recognised as being outside the widget, which was causing the keyboard to be dismissed by the widget.  UIGestureRecognizer doesn't suffer from the issue. Buddy Jean Grizet

--- WrldSearchWidget/private/view/WRLDSearchWidgetView.m
+++ WrldSearchWidget/private/view/WRLDSearchWidgetView.m
@@ -12,44 +12,86 @@
 @end
 
 @implementation WRLDSearchWidgetView
+{
+    UITapGestureRecognizer * m_tapRecogniser;
+}
+
+- (void)willMoveToWindow:(UIWindow *)newWindow
+{
+    if(m_tapRecogniser)
+    {
+        [self.window removeGestureRecognizer: m_tapRecogniser];
+    }
+    
+    m_tapRecogniser = [[UITapGestureRecognizer alloc] initWithTarget: self action: @selector(tapInSubview:)];
+    [m_tapRecogniser setCancelsTouchesInView: NO];
+    [newWindow addGestureRecognizer: m_tapRecogniser];
+}
+
+- (void) tapInSubview: (UIGestureRecognizer *) gestureRecognizer
+{
+    BOOL tapInSubview = [self subview: self.menuSubView containsGesture: gestureRecognizer]           ||
+                        [self subview: self.searchBarSubView containsGesture: gestureRecognizer]      ||
+                        [self subview: self.voiceOptionSubView containsGesture: gestureRecognizer]    ||
+                        [self subview: self.resultsSubView containsGesture: gestureRecognizer]        ||
+                        [self subview: self.suggestionsSubView containsGesture: gestureRecognizer]    ||
+                        [self subview: self.menuContainerSubView containsGesture: gestureRecognizer];
+    
+    if (!tapInSubview)
+    {
+        [self.owner resignFocus];
+    }
+}
+
+- (void)dealloc
+{
+    [self.window removeGestureRecognizer: m_tapRecogniser];
+}
 
 - (BOOL)pointInside:(CGPoint)point withEvent:(UIEvent *)event
 {
-    if([self subviewContainsEvent: self.menuSubView point: point event: event])
+    if([self subview: self.menuSubView containsPoint: point inEvent: event])
     {
         return YES;
     }
-    if([self subviewContainsEvent: self.searchBarSubView point: point event: event])
+    if([self subview: self.searchBarSubView containsPoint: point inEvent: event])
     {
         return YES;
     }
-    if([self subviewContainsEvent: self.voiceOptionSubView point: point event: event])
+    if([self subview: self.voiceOptionSubView containsPoint: point inEvent: event])
     {
         return YES;
     }
-    if([self subviewContainsEvent: self.resultsSubView point: point event: event])
+    if([self subview: self.resultsSubView containsPoint: point inEvent: event])
     {
         return YES;
     }
-    if([self subviewContainsEvent: self.suggestionsSubView point: point event: event])
+    if([self subview: self.suggestionsSubView containsPoint: point inEvent: event])
     {
         return YES;
     }
-    if([self subviewContainsEvent: self.menuContainerSubView point: point event: event])
+    if([self subview: self.menuContainerSubView containsPoint: point inEvent: event])
     {
         return YES;
     }
-    
-    [self.owner resignFocus];
     
     return NO;
 }
 
--(BOOL) subviewContainsEvent: (UIView*) view
-                       point: (CGPoint)point
-                        event: (UIEvent*) event
+- (BOOL) subview: (UIView*) view
+   containsPoint: (CGPoint)point
+         inEvent: (UIEvent*) event
 {
     if (view.userInteractionEnabled && ![view isHidden] && [view pointInside:[self convertPoint:point toView:view] withEvent:event]) {
+        return YES;
+    }
+    return NO;
+}
+
+- (BOOL) subview: (UIView *) view
+ containsGesture: (UIGestureRecognizer *) gestureRecognizer
+{
+    if (view.userInteractionEnabled && ![view isHidden] && CGRectContainsPoint(view.bounds, [gestureRecognizer locationInView:view]) ) {
         return YES;
     }
     return NO;
